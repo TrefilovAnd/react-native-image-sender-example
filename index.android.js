16da87bf166f080c2aedc5ec2fc69bd0524132e9
Трефилов <trefilovandrew@yandex.ru>
2017-02-14 22:23:42 +0500

    lint

--- index.android.js
+++ index.android.js
@@ -25,6 +25,7 @@ var options = {
         path: 'images'
     }
 };
+var URLSERVER = 'http://myserver/photo/upload';
 
 export default class ReactNativeImageSenderExample extends Component {
     constructor(props) {
@@ -32,23 +33,6 @@ export default class ReactNativeImageSenderExample extends Component {
         this.state = {
             avatarSource: { uri: 'https://facebook.github.io/react/img/logo_og.png' }
         };
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Image source={this.state.avatarSource} style={styles.uploadAvatar} />
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.android.js
-                </Text>
-                <TouchableHighlight onPress={this.selectImage.bind(this)}>
-                    <Text style={styles.instructions}>Select Image</Text>
-                </TouchableHighlight>
-            </View>
-        );
     }
 
     selectImage() {
@@ -74,51 +58,52 @@ export default class ReactNativeImageSenderExample extends Component {
                     avatarSource: source
                 });
 
-                var photo = {
+                this.sendPhoto({
                     uri: response.uri,
                     type: response.type,
                     name: response.fileName,
-                };
-
-                var body = new FormData();
-                body.append('upload', photo);
-                body.append('title', 'A beautiful photo!');
-
-                var xhr = new XMLHttpRequest();
-
-                xhr.onreadystatechange = (e) => {
-                    if (xhr.readyState !== 4) {
-                        return;
-                    }
-
-                    if (xhr.status === 200) {
-                        alert('ok');
-                    } else {
-                        console.warn('error');
-                    }
-                };
-
-                xhr.open('POST', 'http://192.168.1.175:2203/58a32e6198ed262784c1f245/photo/upload');
-                xhr.send(body);
-                alert('end');
+                });
             }
         });
     }
 
-    postPhoto(data) {
-        fetch('http://192.168.1.175:2203/58a32e6198ed262784c1f245/photo/upload', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'multipart/form-data',
-            },
-            body: new FormData(data)
-        })
-            .then((response) => response.json())
-            .then((responseJson) => alert(responseJson.status))
-            .catch((error) => {
-                console.error(error);
-            });
+    sendPhoto(photo) {
+        var body = new FormData();
+        var xhr = new XMLHttpRequest();
+
+        body.append('upload', photo);
+
+        xhr.onreadystatechange = (e) => {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+
+            if (xhr.status === 200) {
+                console.log('Image sent');
+            } else {
+                console.warn('error');
+            }
+        };
+
+        xhr.open('POST', URLSERVER);
+        xhr.send(body);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={this.state.avatarSource} style={styles.uploadAvatar} />
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                </Text>
+                <TouchableHighlight onPress={this.selectImage.bind(this)}>
+                    <Text style={styles.instructions}>Select Image</Text>
+                </TouchableHighlight>
+            </View>
+        );
     }
 }
 
